ddb3c778084d9445eb030fa5545510bb84d02f29
anonymityqw <wangyanqingchn@163.com>
2017-11-07 10:10:18 +0800

    change readme.md

--- CSLT_Node_Sparseness_Toolkit/prune_node/run_several_pct.sh
+++ CSLT_Node_Sparseness_Toolkit/prune_node/run_several_pct.sh
@@ -1,10 +1,10 @@
 # To run this script, run in terminal: nohup bash ./run_several_pct.sh > ../../../../log4/1.out &
-# In most conditions, you need to change the paras in three places: max_id, x, x
+# In most conditions, you need to change the paras in three places: no, first_no, last_no
 
-max_id=12;
+max_id=no;
 
 # generate final_new.mdl in each folders
-for x in {12..12}; do
+for x in {first_no..last_no}; do
 {
   cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/prune_${x};
   cp ../baseline/final_v_baseline_format.raw .
@@ -18,66 +18,3 @@ for x in {12..12}; do
   bash sparse_rate_layer.sh &
 }
 done
-#
-## decode and find wer
-#for x in {10..10}; do
-#{
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/prune_${x};
-#  
-#  # copy log.txt as final.mdl
-#  cp final_new.mdl ../../final.mdl;
-#
-#  # decode
-#  echo "----- start decoding now -----"
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5;
-#  ./run_tdnn_3_decode.sh;
-#  echo "----- end decoding now ------"
-#
-#  # 
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3;
-#  bash ./new_decode_file_node.sh ${x};
-#}
-#done
-#
-## combine the strategies to a file
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/strategy;
-#  bash combine.sh $max_id;
-# 
-## combine the best_wer to a file
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3;
-#  bash combine_best_wer.sh $max_id;
-#
-## combine sparse_rate to a file
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/sparse_rate;
-#  bash combine_sparse_rate.sh $max_id;
-#
-## combination of the final results
-#  cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/results;
-#  
-#  if [ -f strategy ]; then
-#    rm strategy;
-#  fi
-#
-#  cp ../strategy/strategy ./strategy
-#  
-#  if [ -f best_wer ]; then
-#    rm best_wer;
-#  fi
-#
-#  cp /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/best_wer ./best_wer
-#
-#  if [ -f sparse_rate ]; then
-#    rm sparse_rate;
-#  fi
-#
-#  cp /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/sparse_rate/sparse_rate ./sparse_rate;
-#
-#  if [ -f sparse_rate_format ]; then
-#    rm sparse_rate_format;
-#  fi
-#
-#  cp /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune_node/sparse_rate/sparse_rate_format ./sparse_rate_format
-#
-## combination of "results_format"
-#cd /work7/wangyanqing/kaldi/egs/wsj/s5/exp/nnet3/nnet_tdnn_a_3/prune/results;
-#bash combine_format.sh
